fe04f623d0a749cdc3bead1bf0a457357f0a716c
Alfonso Rojas-Alvarez <rojas.alfonso@me.com>
2016-03-25 13:28:31 -0600

    Update Whatsapp.R
    Some formatting adjustments.

--- Google Drive/JHU/Whatsapp.R
+++ Google Drive/JHU/Whatsapp.R
@@ -2,8 +2,8 @@
 ## You need to export the chat from Whatsapp, and clean the data in Excel, then save as .csv
 
 rm(list = ls())
-
 library(ggplot2)
+
 # Import 
 chat <- read.csv("~/Desktop/chat.csv", header = TRUE)
 head(chat)
@@ -15,13 +15,13 @@ chat$Fecha <- sub(',', '', chat$Fecha)
 # Format as Date Time
 
 chat$Fecha <- as.Date(chat$Fecha, "%m/%d/%y")
-chat$X <- paste(chat$Fecha, chat$Hora, sep=" ")
-chat$X <- strptime(chat$X, "%Y-%m-%d %H:%M:%S")
+chat$Fecha_working <- paste(chat$Fecha, chat$Hora, sep=" ")
+chat$Fecha_working <- strptime(chat$Fecha_working, "%Y-%m-%d %H:%M:%S")
 
-chat$Mes <- months(chat$X, abbreviate = TRUE)
-chat$Dia <- weekdays(chat$X, abbreviate = TRUE)
-chat$Hora <- format(chat$X, "%H")
-chat$X <- as.Date(as.character(chat$X), format = "%Y-%m-%d")
+chat$Mes <- months(chat$Fecha_working, abbreviate = TRUE)
+chat$Dia <- weekdays(chat$Fecha_working, abbreviate = TRUE)
+chat$Hora <- format(chat$Fecha_working, "%H")
+chat$Fecha_working <- as.Date(as.character(chat$X), format = "%Y-%m-%d")
 
 ggplot(chat, aes( x = Autor, fill = Autor)) + geom_histogram(binwidth = 30) + ggtitle("Cantidad de Mensajes por Autor") + labs(y = "Mensajes Enviados")
 ggplot(chat, aes( x = Fecha, fill = Autor)) + geom_histogram(binwidth = 30) + ggtitle("Cantidad de Mensajes por Mes") + labs(y = "Mensajes Enviados")
